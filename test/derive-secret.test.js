86ff945030717dbed71d13ce3ec8331be7cf45ec
mixmix <whimful@gmail.com>
2021-08-09 13:01:45 +1200

    prove derive-secret can take String|Buffer labels

--- test/derive-secret.test.js
+++ test/derive-secret.test.js
@@ -1,6 +1,8 @@
+/* eslint-disable camelcase */
+
 const test = require('tape')
 const vectors = [
-  require('envelope-spec/vectors/derive_secret1.json'),
+  require('envelope-spec/vectors/derive_secret1.json')
 ]
 const labels = require('envelope-spec/derive_secret/constants.json')
 
@@ -19,12 +21,25 @@ test('derive-secret', t => {
     //   └──> body_key
     const read_key = derive(msg_key, [labels.read_key])
     const header_key = derive(read_key, [labels.header_key])
-    const body_key   = derive(read_key, [labels.body_key])
+    const body_key = derive(read_key, [labels.body_key])
 
     t.deepEqual(read_key, vector.output.read_key, 'derive read_key')
     t.deepEqual(header_key, vector.output.header_key, 'derive header_key')
     t.deepEqual(body_key, vector.output.body_key, 'derive body_key')
+
+    console.log('and with Buffer labels:')
+    const read_key2 = derive(msg_key, [toBuffer(labels.read_key)])
+    const header_key2 = derive(read_key, [toBuffer(labels.header_key)])
+    const body_key2 = derive(read_key, [toBuffer(labels.body_key)])
+
+    t.deepEqual(read_key2, vector.output.read_key, 'derive read_key')
+    t.deepEqual(header_key2, vector.output.header_key, 'derive header_key')
+    t.deepEqual(body_key2, vector.output.body_key, 'derive body_key')
   })
 
   t.end()
 })
+
+function toBuffer (str) {
+  return Buffer.from(str, 'utf8')
+}
