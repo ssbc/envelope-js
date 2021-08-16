13961a2ae946b440e8f61b1ec9737247d49043f6
mixmix <whimful@gmail.com>
2021-08-16 12:07:10 +1200

    update ssb-bfe, tidy util/bfe-nil-msg code

--- util/bfe-nil-msg.js
+++ util/bfe-nil-msg.js
@@ -1,31 +1,32 @@
 const { bfeNamedTypes } = require('ssb-bfe')
 const error = require('./error')
 
-const GENERIC = bfeNamedTypes['generic']
-const NIL_TFD = Buffer.from([GENERIC.code, GENERIC.formats['nil'].code])
-
-const FEED = bfeNamedTypes['feed']
-const CLASSIC_FEED_TF = Buffer.from([FEED.code, FEED.formats['ssb/classic'].code])
-const GABBYGR_FEED_TF = Buffer.from([FEED.code, FEED.formats['ssb/gabby-grove'].code])
-const BENDYBT_FEED_TF = Buffer.from([FEED.code, FEED.formats['ssb/bendy-butt'].code])
-
-const MSG = bfeNamedTypes['msg']
-const CLASSIC_MSG_TF = Buffer.from([MSG.code, MSG.formats['ssb/classic'].code])
-const GABBYGR_MSG_TF = Buffer.from([MSG.code, MSG.formats['ssb/gabby-grove'].code])
-const BENDYBT_MSG_TF = Buffer.from([MSG.code, MSG.formats['ssb/bendy-butt'].code])
-
-module.exports = function convertBFENilMsg(msgTFD, feedTFK) {
-  if (msgTFD.equals(NIL_TFD)) {
-    const feedTF = feedTFK.slice(0, 2)
-    if (feedTF.equals(BENDYBT_FEED_TF))
-      return Buffer.concat([BENDYBT_MSG_TF, Buffer.alloc(32)])
-    else if (feedTF.equals(GABBYGR_FEED_TF))
-      return Buffer.concat([GABBYGR_MSG_TF, Buffer.alloc(32)])
-    else if (feedTF.equals(CLASSIC_FEED_TF))
-      return Buffer.concat([CLASSIC_MSG_TF, Buffer.alloc(32)])
-    else
-      throw error('Unknown feed type', feedTF)
-  } else
-    return msgTFD
+const toTF = (typeName, formatName) => {
+  return Buffer.from([
+    bfeNamedTypes[typeName].code,
+    bfeNamedTypes[typeName].formats[formatName].code
+  ])
 }
 
+const NIL_TFD = toTF('generic', 'nil')
+
+const CLASSIC_FEED_TF = toTF('feed', 'classic')
+const GABBYGR_FEED_TF = toTF('feed', 'gabby-grove')
+const BENDYBT_FEED_TF = toTF('feed', 'bendy-butt')
+
+const CLASSIC_MSG_TF = toTF('msg', 'classic')
+const GABBYGR_MSG_TF = toTF('msg', 'gabby-grove')
+const BENDYBT_MSG_TF = toTF('msg', 'bendy-butt')
+
+const ZEROS = Buffer.alloc(32)
+
+module.exports = function convertBFENilMsg (msgTFD, feedTFK) {
+  if (!msgTFD.equals(NIL_TFD)) return msgTFD
+
+  const feedTF = feedTFK.slice(0, 2)
+  if (feedTF.equals(CLASSIC_FEED_TF)) return Buffer.concat([CLASSIC_MSG_TF, ZEROS])
+  if (feedTF.equals(GABBYGR_FEED_TF)) return Buffer.concat([GABBYGR_MSG_TF, ZEROS])
+  if (feedTF.equals(BENDYBT_FEED_TF)) return Buffer.concat([BENDYBT_MSG_TF, ZEROS])
+
+  throw error('Unknown feed type', feedTF)
+}
