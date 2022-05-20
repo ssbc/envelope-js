343428f0663af955134e078b0d8dc93d06ab543e
Anders Rune Jensen <arj03@protonmail.ch>
2022-05-20 23:35:20 +0200

    Support buttwoo

--- util/bfe-nil-msg.js
+++ util/bfe-nil-msg.js
@@ -6,10 +6,12 @@ const NIL_TFD = toTF('generic', 'nil')
 const CLASSIC_FEED_TF = toTF('feed', 'classic')
 const GABBYGR_FEED_TF = toTF('feed', 'gabbygrove-v1')
 const BENDYBT_FEED_TF = toTF('feed', 'bendybutt-v1')
+const BUTTWOO_FEED_TF = toTF('feed', 'buttwoo-v1')
 
 const CLASSIC_MSG_TF = toTF('message', 'classic')
 const GABBYGR_MSG_TF = toTF('message', 'gabbygrove-v1')
 const BENDYBT_MSG_TF = toTF('message', 'bendybutt-v1')
+const BUTTWOO_MSG_TF = toTF('message', 'buttwoo-v1')
 
 const ZEROS = Buffer.alloc(32)
 
@@ -20,6 +22,7 @@ module.exports = function convertBFENilMsg (msgTFD, feedTFK) {
   if (feedTF.equals(CLASSIC_FEED_TF)) return Buffer.concat([CLASSIC_MSG_TF, ZEROS])
   if (feedTF.equals(GABBYGR_FEED_TF)) return Buffer.concat([GABBYGR_MSG_TF, ZEROS])
   if (feedTF.equals(BENDYBT_FEED_TF)) return Buffer.concat([BENDYBT_MSG_TF, ZEROS])
+  if (feedTF.equals(BUTTWOO_FEED_TF)) return Buffer.concat([BUTTWOO_MSG_TF, ZEROS])
 
   throw error('Unknown feed type', feedTF)
 }
